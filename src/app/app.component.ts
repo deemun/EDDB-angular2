291c6fc810da1fa3452b644feaaaedcb34af43bf
deemun <sio_lab@siodigitalsair.bcricwh.lan>
2017-01-11 11:13:40 -0800

    made both components one, add functionality incoming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Resources } from './app.data';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'app works!';
+  tabs = ["tab1", "tab2", "tab3"]
+  currentTab = this.tabs[0];
+  currentResources = Resources[this.currentTab]; //making sure the resources come under the right tabName
+  
+
+
+  clicked(tab){
+    this.currentTab = tab;
+    this.currentResources = Resources[this.currentTab];
+  }
+
+  
+
+  
+
+
+
+ 
+
+
+
+
+
 }
+
+
